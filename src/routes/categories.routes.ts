30c71a8bb4e66df03a562c0f8e9bbb4237799551
Guilherme Figueiredo <guilherme.figueiredo092@gmail.com>
2022-08-24 14:08:25 -0300

    creating file uploads

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const uplodad = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -12,5 +17,15 @@ categoriesRoutes.post('/', (request, response) => {
 categoriesRoutes.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post(
+  '/import',
+  uplodad.single('file'),
+  (request, response) => {
+    const { file } = request;
+    console.log(file);
+    return response.send();
+  }
+);
 
 export { categoriesRoutes };
