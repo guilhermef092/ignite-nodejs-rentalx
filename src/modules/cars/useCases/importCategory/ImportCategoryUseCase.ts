5269d5f108f7ba295e464e602dc584037fe67bf0
Guilherme Figueiredo <guilherme.figueiredo092@gmail.com>
2022-08-24 15:15:21 -0300

    Upload categories

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,50 @@
+import { parse } from 'csv-parse';
+import fs from 'fs';
+
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoryUseCase {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private categoriesRepository: ICategoriesRepository) { }
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+
+      const categories: IImportCategory[] = [];
+
+      const parseFile = parse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async excecute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    console.log(categories);
+  }
+}
+
+export { ImportCategoryUseCase };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
@@ -22,9 +23,7 @@ categoriesRoutes.post(
   '/import',
   uplodad.single('file'),
   (request, response) => {
-    const { file } = request;
-    console.log(file);
-    return response.send();
+    importCategoryController.handle(request, response);
   }
 );
 
