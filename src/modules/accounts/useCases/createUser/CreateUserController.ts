3295ec5428119def40b76da2c28dc398e99e26c6
Guilherme Figueiredo <guilherme.figueiredo092@gmail.com>
2022-09-07 09:38:39 -0300

    Migration alteruser

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -5,13 +5,12 @@ import { CreateUserUserCase } from './CreateUserUseCase';
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, username, email, password, driver_license } = request.body;
+    const { name, email, password, driver_license } = request.body;
 
     const createUserUseCase = container.resolve(CreateUserUserCase);
 
     await createUserUseCase.execute({
       name,
-      username,
       email,
       password,
       driver_license,
