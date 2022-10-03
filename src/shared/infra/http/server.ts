1dcdd6e79d515d5cdbddbdd9dac7de09c804d179
Guilherme Figueiredo <guilherme.figueiredo092@gmail.com>
2022-10-03 18:31:31 -0300

    Middleware admin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,12 +2,12 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import '@shared/container';
 import { AppError } from '@shared/errors/AppError';
 import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
 import { router } from './routes';
+import '@shared/container';
 
 createConnection();
 const app = express();
